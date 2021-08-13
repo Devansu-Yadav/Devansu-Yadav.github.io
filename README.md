98e3ad9b0c05e0bbb045df3334d1dc23cf4d270c
Devansu <devansuyadav@gmail.com>
2021-08-13 18:26:00 +0530

    Update: Updated with Latest portfolio design

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# My Portfolio Website containing Projects and Resume
+# My Personal Portfolio
+
+<h1 align="center">Hi <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="29px">, I'm Devansu Yadav</h1>
+<h2 align="center">I am a Web Developer🕸 and I aspire to become a Software Developer👨‍💻</h2>
+<h2 align="center">I am also an <b>Alpha Microsoft Learn Student Ambassador</b></h2>
---
+++ src/js/darkMode.js
@@ -0,0 +1,25 @@
+const toggle = document.querySelector(".toggle");
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+
+if(currentTheme == "dark") {
+    document.body.classList.toggle("dark-mode");
+} else if(currentTheme == "light") {
+    document.body.classList.toggle("light-mode");
+}
+
+toggle.addEventListener("click", () => {
+    if(prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-mode");
+
+        var theme = document.body.classList.contains("light-mode")? "light" : "dark";
+    } else {
+        document.body.classList.toggle("dark-mode");
+
+        var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    }
+    
+    localStorage.setItem("theme", theme);
+});
